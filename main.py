16f4515b8e73087068b5a0a1a2e0ab3848ff33d3
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-11-08 11:25:25 +0100

    Main app

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import routers.driver_router
 import routers.passenger_router
 import routers.reservation_router
 import routers.trip_router
+import routers.auth_router
+import routers.stripe_router
 
 app = FastAPI(
     title="Carpooling API",
@@ -17,9 +19,11 @@ app = FastAPI(
 
 
 # Montez les routeurs sur l'application
-app.include_router(routers.driver_router.router)
+app.include_router(routers.driver_router.router, prefix="/api")
 app.include_router(routers.passenger_router.router, prefix="/api")
 app.include_router(routers.reservation_router.router, prefix="/api")
 app.include_router(routers.trip_router.router, prefix="/api")
+app.include_router(routers.auth_router.router, prefix="/api")
+app.include_router(routers.stripe_router.router, prefix="/api")
 # app.include_router(routers.payment_router, prefix="/api")
 
