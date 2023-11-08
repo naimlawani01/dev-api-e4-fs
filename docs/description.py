a26d56c05dbf2bc143d737008acc6bbe8a4649c6
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-11-08 11:24:48 +0100

    Description

--- docs/description.py
+++ docs/description.py
@@ -1,1 +1,4 @@
-api_description="A FastAPI application for managing carpooling services."
+api_description=""" 
+A FastAPI application for managing carpooling services.
+1. Laisser vous rediriger sur le lien du [checkout](/api/stripe/checkout) 
+"""
