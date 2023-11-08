86e5143425b237a210f29b95f514f5155c31f51e
Lawani Imrane <48188711+naimlawani01@users.noreply.github.com>
2023-11-08 11:24:24 +0100

    Database and passenger router

---
+++ configs/firebase_config_example.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    "apiKey": "AIzaSyBXgaQ-rJHrhoKswtYulBTdg_I9lGfDWGE",
+    "authDomain": "carpooling-cd0af.firebaseapp.com",
+    "databaseURL": "https://carpooling-cd0af-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "carpooling-cd0af",
+    "storageBucket": "carpooling-cd0af.appspot.com",
+    "messagingSenderId": "761815739411",
+    "appId": "1:761815739411:web:bd78609880567f6fa4e3e6"
+}
--- routers/passenger_router.py
+++ routers/passenger_router.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from typing import List
+import uuid
+from firebase_admin import auth
+from database.firebase import authSession
+from routers.auth_router import get_current_user
 # from models import Passenger
-from schemas.schemas_dto import Passenger
+from schemas.schemas_dto import Passenger, Passenger_POST
+
+#Database
+from database.firebase import db
 
 router = APIRouter(
     prefix='/passengers',
@@ -13,22 +20,50 @@ def get_passengers():
     # Logique pour obtenir la liste des passagers
     pass
 
-@router.get("/{passenger_id}", response_model=Passenger)
+@router.get("/{passenger_id}", status_code=200)
 def get_passenger_by_id(passenger_id: int):
-    # Logique pour obtenir un passager par ID
-    pass
+    # Logique pour obtenir un conducteur par ID
+    fireBaseobject = db.child("passenger").child(passenger_id).get().val()
+    # resultArray = [value for value in fireBaseobject.values()]
+    return fireBaseobject
 
-@router.post("/", response_model=Passenger)
-def create_passenger(passenger: Passenger):
-    # Logique pour créer un passager
-    pass
+@router.post("/", status_code=201)
+def create_passenger(passenger_data: Passenger_POST):
+    
+    generated_id = uuid.uuid4()
+    try:
+        user = auth.create_user(
+            email = passenger_data.email,
+            password = passenger_data.password
+        )
+        new_passager = Passenger(passenger_id=str(generated_id), first_name=passenger_data.first_name, last_name=passenger_data.last_name, user_id= str(user.uid))
+        db.child("passenger").push(new_passager.model_dump())
+        return {
+        "message": f"Nouvel utilisateur créé avec id : {user.uid}"
+        }
+    except auth.EmailAlreadyExistsError:
+        raise HTTPException(
+            status_code=409,
+            detail=f"Un compte existe déjà pour : {passenger_data.email}"
+        )
 
 @router.put("/{passenger_id}", response_model=Passenger)
 def update_passenger(passenger_id: int, passenger: Passenger):
     # Logique pour mettre à jour un passager
     pass
 
-@router.delete("/{passenger_id}", response_model=dict)
-def delete_passenger(passenger_id: int):
-    # Logique pour supprimer un passager
-    pass
+@router.delete("/{passenger_id}", status_code=204)
+def delete_passenger(passenger_id: str, userData: int = Depends(get_current_user)):
+    try:
+        fireBaseobject = db.child("passenger").child(passenger_id).get(userData['idToken']).val()
+    except:
+        raise HTTPException(
+            status_code=403, detail="Accès interdit"
+        )
+    if fireBaseobject is not None:
+        if fireBaseobject['user_id'] == userData['user_id']:
+            return db.child("passenger").child(passenger_id).remove(userData['idToken'])
+        return {
+            "message": "Vous n'avez pas le droit de supprimer ce passager"
+        }
+    raise HTTPException(status_code= 404, detail="Passenger not found")
